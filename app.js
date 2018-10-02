ba010ceed833663a02a5da1bca6568a36cc05986
kirin111697 <kirinonfire6373@gmail.com>
2018-10-02 23:00:46 +0800

    hw3 ver.2

--- app.js
+++ app.js
@@ -3,7 +3,80 @@ var delay = function(s){
         setTimeout(resolve,s); 
     });
 };
+
+function gtsLoad() {
+    delay(1000).then(function(){
+        document.getElementById('gtsP').style.display = "flex";
+        document.getElementById('mainP').style.display = "none";
+        document.getElementById('mainP').style.top = "10%";
+        $('#gtsP > div.text').animate({
+            'top': '10%',
+            'opacity': '1',
+        }, 500, 'easeInQuint');
+        return delay();
+    }).then(function(){
+        $('#gtspic1').animate({
+            'width': '45%',
+        }, 1500, 'easeInQuint');  
+        return delay(1500);
+    }).then(function(){
+        $('#colorbg').animate({
+            'width': '42%',
+        }, 1000, 'easeInQuint');
+        delay(500);
+        $('#gtspic2').animate({
+            'width': '42%',
+        }, 1500, 'easeInQuint');
+        return delay(2000);
+    }).then(function(){
+        document.getElementById('gtsP').style.top = "-100%";
+        delay(1000).then(function(){
+            _init();
+            mainLoad();
+        });
+    });
+}
   
+function mainLoad(){
+    delay().then(function(){
+        document.getElementById('mainP').style.display = "flex";
+        document.getElementById('gtsP').style.top = "10%";
+        $('#mpic').animate({
+            'width': '50%',
+        }, 1500, 'easeInQuint');  
+        return delay(1000);
+    }).then(function(){
+        $('#mainP > div.text').animate({
+            'top': '15%',
+            'opacity': '1',
+        },500);
+        return delay(1000); 
+    }).then(function(){
+        $('#dot').animate({
+            'opacity': '0.2',
+        },1000);
+        $('#mpic2').animate({    
+            'width': '15%',
+        }, 1000);
+        return delay(2000);
+    }).then(function(){
+        document.getElementById('mainP').style.top = "-100%";
+        gtsLoad();
+    });
+}
+
+function _init(){
+    document.getElementById('mpic').style.width = "0%";
+    document.querySelector('#mainP > div.text').style.top = "25%";
+    document.querySelector('#mainP > div.text').style.opacity = "0";
+    document.getElementById('mpic2').style.width = "0%";
+    document.querySelector('#gtsP > div.text').style.top = "20%";
+    document.querySelector('#gtsP > div.text').style.opacity = "0";
+    document.getElementById('gtspic1').style.width = "0%";
+    document.getElementById('gtspic2').style.width = "0%";
+    document.getElementById('colorbg').style.width = "0%";
+}
+
 delay().then(function(){
     $('#menuicon').animate({
         'width': '100%',
@@ -23,24 +96,9 @@ delay().then(function(){
     },500, 'easeInQuint');
     return delay(500);
 }).then(function(){
-    $('#mpic').animate({
-        'width': '50%',
-    }, 1500, 'easeInQuint');  
-    return delay(1000);
-}).then(function(){
-    $('.text').animate({
-        'top': '10%',
-        'opacity': '1',
-    },1000);
-    return delay(1000); 
-}).then(function(){
-    $('#dot').animate({
-        'opacity': '0.1',
-    },1000);
-    $('#mpic2').animate({    
-        'width': '15%',
-    }, 1000);
+    mainLoad();
 });
+
 
 
 
